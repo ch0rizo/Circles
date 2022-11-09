d137eb893c69b8520ff5dd17ea6335dbd0bf4439
Christoffer Nguyen <christofferhoang@gmail.com>
2022-11-09 11:35:31 +0200

    SEED: users and circles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,75 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "> Deleting the DB.."
+CircleEvent.destroy_all
+UserCircle.destroy_all
+Event.destroy_all
+Circle.destroy_all
+User.destroy_all
+
+puts "> Creating the main character..."
+
+main_user = User.create(email: 'benten@gmail.com', password: 'password', username: 'benten_', first_name: 'Ben', last_name: 'Ten')
+
+
+puts "> Creating other random shit"
+50.times do
+  User.create(
+    email: Faker::Internet.email,
+    password: 'password',
+    username: Faker::Internet.username,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name
+  )
+end
+
+# Family circle
+circle1 = Circle.create(
+  name: 'Family',
+  private: true
+)
+UserCircle.create(
+  user_id: main_user.id,
+  circle_id: circle1.id
+)
+4.times do
+  UserCircle.create(
+    user_id: User.all.sample.id,
+    circle_id: circle1.id
+  )
+end
+
+
+# Fotball circle
+circle2 = Circle.create(
+  name: 'MLC FC',
+  private: true
+)
+UserCircle.create(
+  user_id: main_user.id,
+  circle_id: circle2.id
+)
+12.times do
+  UserCircle.create(
+    user_id: User.all.sample.id,
+    circle_id: circle2.id
+  )
+end
+
+
+# Work circle
+circle3 = Circle.create(
+  name: 'Work',
+  private: false
+)
+UserCircle.create(
+  user_id: main_user.id,
+  circle_id: circle3.id
+)
+10.times do
+  UserCircle.create(
+    user_id: User.all.sample.id,
+    circle_id: circle3.id
+  )
+end
+
+puts '> Finished!'
+puts "> Made #{User.count} users, #{Circle.count} circles"
