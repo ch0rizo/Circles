459824b440b49074ec296088effd715deddbbb93
Christoffer Nguyen <christofferhoang@gmail.com>
2022-11-09 12:23:24 +0200

    FIX: model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,13 @@
 class User < ApplicationRecord
-  has_many :circles
-  has_many :events
-  
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
   has_many :user_events
   has_many :events, through: :user_events
   has_many :circle_messages
   has_many :event_messages
   has_many :user_circles
   has_many :circles, through: :user_circles
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
