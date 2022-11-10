620271a5aa419606c752d66526b89f3a8304d076
Christoffer Nguyen <christofferhoang@gmail.com>
2022-11-10 17:30:21 +0200

    FEAT: primitive chatbook check

--- app/controllers/circle_messages_controller.rb
+++ app/controllers/circle_messages_controller.rb
@@ -1,2 +1,19 @@
 class CircleMessagesController < ApplicationController
+  def create
+    @circle = Circle.find(params[:circle_id])
+    @circle_message = CircleMessage.new(circle_message_params)
+    @circle_message.circle = @circle
+    @circle_message.user = current_user
+    if @circle_message.save
+      redirect_to @circle
+    else
+      render "chatrooms/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def circle_message_params
+    params.require(:circle_message).permit(:content)
+  end
 end
