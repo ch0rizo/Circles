3a86d1f1104f4a49a08582fd99e5682102097a50
Nicholas Wickmann <nicowick@icloud.com>
2022-11-11 15:52:31 +0200

    Map sucks

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -12,26 +12,13 @@ export default class extends Controller {
   connect() {
     console.log("hi")
     mapboxgl.accessToken = this.apiKeyValue
-
-    // this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-    // mapboxgl: mapboxgl }))
-
-    // this.map = new mapboxgl.Map({
-    //   container: this.element,
-    //   style: "mapbox://styles/pdunleav/cjofefl7u3j3e2sp0ylex3cyb" // <-- use your own!
-    // });
+    
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
     })
 
-    // this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-    // mapboxgl: mapboxgl }))
 
-    //   this.map = new mapboxgl.Map({
-    //   container: this.element,
-    //   style: "mapbox://styles/mapbox/streets-v10"
-    // })
 
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
@@ -42,15 +29,6 @@ export default class extends Controller {
     this.markersValue.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window)
 
-      // Create a HTML element for your custom marker
-      // const customMarker = document.createElement("div")
-      // customMarker.className = "marker"
-      // customMarker.style.backgroundImage = `url('${marker.image_url}')`
-      // customMarker.style.backgroundSize = "contain"
-      // customMarker.style.width = "25px"
-      // customMarker.style.height = "25px"
-
-      // Pass the element as an argument to the new marker
       new mapboxgl.Marker()
         .setLngLat([marker.lng, marker.lat])
         // .setPopup(popup)
