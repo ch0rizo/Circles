d410d62eb734da9180cc3e611ee55e9c9a895357
tudor ojica <tudor.ojica@gmail.com>
2022-11-11 15:39:10 +0100

    flatpickr

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,5 +7,8 @@ import { application } from "./application"
 import CircleChatroomSubscriptionController from "./circle_chatroom_subscription_controller"
 application.register("circle-chatroom-subscription", CircleChatroomSubscriptionController)
 
+import FlatpickerController from "./flatpicker_controller"
+application.register("flatpicker", FlatpickerController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
