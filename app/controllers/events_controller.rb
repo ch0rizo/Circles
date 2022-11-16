6674936a40574123fefeea7a087d1d3181765489
Liam Strickland <liam.strickland96@gmail.com>
2022-11-16 20:00:44 +0200

    preparations have been made for the splitty

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,7 +11,12 @@ class EventsController < ApplicationController
     @event.user = current_user
     @event.private = params[:event][:private][1]
     if @event.save
-      redirect_to root_path, notice: "Event created!"
+      circle = @event.circle_events.first.circle
+      circle.users.each do |user|
+        user_event = UserEvent.new(event: @event, user: user)
+        user_event.save!
+      end
+      redirect_to event_path(@event), notice: "Event created!"
     else
       redirect_to root_path, notice: "Event not created, please try again"
     end
