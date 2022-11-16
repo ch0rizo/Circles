1a6aa12c39f3d6b3fcae5a01d1b4d7e213dfa970
Christoffer Nguyen <christofferhoang@gmail.com>
2022-11-16 13:17:39 +0200

    fix????

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @all_events = Event.all
     @events = Event.where(user_id: @user)
     @event = @events[0]
     @circles = @user.circles
