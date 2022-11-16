1cc0ebf49889f9085165346858866291b1ae3695
Christoffer Nguyen <christofferhoang@gmail.com>
2022-11-16 12:46:16 +0200

    dashboard layout finished (hopefully)

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,7 @@ class DashboardController < ApplicationController
   def show
     @user = User.find(params[:id])
     @events = Event.where(user_id: @user)
+    @event = @events[0]
+    @circles = @user.circles
   end
 end
