32f4d18386b489aec7220e5d70fa6cc7b5855cf6
Christoffer Nguyen <christofferhoang@gmail.com>
2022-11-16 18:34:45 +0200

    STYLE: fix footer on style page

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ User.destroy_all
 
 puts "> Creating the main character..."
 
-main_user = User.create(email: 'benten@gmail.com', password: 'password', username: 'benten', first_name: 'Ben', last_name: 'Ten')
+main_user = User.create(email: 'benten@gmail.com', password: 'password', username: 'benten', first_name: 'Ben', last_name: 'Lee')
 user_url = "https://res.cloudinary.com/dhyxfzmqi/image/upload/v1668172413/development/julian-wan-WNoLnJo7tS8-unsplash_svo56p.jpg"
 user_file = URI.open(user_url)
 main_user.photo.attach(io: user_file, filename: 'circle_img.png', content_type: 'image/png')
