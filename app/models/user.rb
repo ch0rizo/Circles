588f7e9fca272acc67051f549dea55262c65d800
Christoffer Nguyen <christofferhoang@gmail.com>
2022-11-17 18:24:19 +0200

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :circles, through: :user_circles
   has_one_attached :photo
 
+  validates :first_name, uniqueness: true
+  validates :last_name, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
